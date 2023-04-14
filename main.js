74062e101d6b519bf3519082c23553514535e240
the-man2004 <heinrichhb111@gmail.com>
2023-04-14 16:38:37 -0700

    added popp functionality

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const burgerMenu = document.querySelector(".burger-menu");
+const burgerPopup = document.querySelector(".burger-popup");
+const navbar = document.querySelector(".navbar");
+const noteSelector = document.querySelector(".note-selector");
+const notePopup = document.querySelector(".note-popup");
+
+// event listeners
+navbar.addEventListener("click", function (e) {
+  const target = e.target.classList;
+  // burger popup
+  if (target.contains("burger-menu")) {
+    burgerPopup.classList.toggle("no-display");
+  }
+
+  // note popup
+  if (target.contains("note-selector")) {
+    notePopup.classList.toggle("no-display");
+  }
+});
